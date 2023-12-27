6a981400a282b1ec909d5c434c53f1108ecbbfcd
Shinji404 <20521981@gm.uit.edu.vn>
2023-12-27 11:22:18 +0700

    Create files and Making a navigation bar

---
+++ src/components/Navigation.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Nav, Navbar, NavDropdown, Container, Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+function Naviagtion() {
+    return (
+        <Navbar bg="light" expand="lg" className="bg-body-tertiary">
+          <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+
+              <Nav className="ms-auto">
+                <LinkContainer to="/login">
+                  <Nav.Link>Login</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/chat">
+                  <Nav.Link>Chat</Nav.Link>
+                </LinkContainer>
+                
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      );
+      
+    }
+
+export default Naviagtion;
