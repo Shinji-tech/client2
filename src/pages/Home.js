937a1fcf88ebc38ed7004a729f8841d87e96fb10
Shinji404 <20521981@gm.uit.edu.vn>
2023-12-27 15:30:34 +0700

    Create and edit home page (Adding button and link to chat)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import { Row, Col, Button} from "react-bootstrap";
+import { LinkContainer } from 'react-router-bootstrap';
 const Home = () => {
   return (
-    <div>Home</div>
+    <Row>
+        <Col md={6} className='d-flex flex-direction-column align-items-center justify-content-center'>
+            <div>
+                <h1>Share the world with your friends</h1>
+                <p>Travel beyond the world with Chat App</p>
+                <LinkContainer to="/chat">
+                    <Button variant='success'>
+                        Get Started Now!
+                    <i className='fas fa-comments home-message-icon'></i>
+                    </Button>
+                </LinkContainer>
+                </div>       
+        </Col>
+    </Row>
   )
 }
 
