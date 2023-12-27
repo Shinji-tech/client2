6f391d0f8a2cf531ed83b87aa927b528207cf881
Shinji404 <20521981@gm.uit.edu.vn>
2023-12-27 22:58:08 +0700

    Changing Port and updating messageform

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -42,7 +42,7 @@ function Sidebar() {
     });
 
     function getRooms() {
-        fetch("http://localhost:5000/rooms")
+        fetch("http://localhost:5001/rooms")
             .then((res) => res.json())
             .then((data) => setRooms(data));
     }
