5de01affcc47d141941ed03bbd86a8974d6fac65
Shinji404 <20521981@gm.uit.edu.vn>
2023-12-27 21:14:05 +0700

    Making Form and Side bar

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,9 +1,33 @@
 import React from 'react'
-
-const MessageForm = () => {
+import { Button, Form, Row, Col } from "react-bootstrap";
+import './MessageForm.css';
+    function MessageForm() {
+        function handleSubmit(e) {
+            e.preventDefault();
+        }
   return (
-    <div>MessageForm</div>
-  )
+    <>
+    <div className='message-output'></div>
+
+        <Form onSubmit={handleSubmit}>
+
+            <Row>
+                <Col md={11}>
+
+                    <Form.Group>
+                        <Form.Control type="text" placeholder='Your message'></Form.Control>
+                    </Form.Group>
+                </Col>
+                <Col md={1}>
+                    <Button variant='primary' type='submit' style={{width:'100%', backgroundColor: 'orange'}}>
+                        <i className='fas fa-paper-plane'></i>
+                    </Button>
+                </Col>
+            </Row>
+        </Form>
+    </>
+    
+    );
 }
 
-export default MessageForm
+export default MessageForm;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { ListGroup } from 'react-bootstrap';
 
 const Sidebar = () => {
+    const rooms = ["first room", "secon room", "third room"]
   return (
-    <div>Sidebar</div>
+    <>
+        <h2>Available Rooms</h2>
+        <ListGroup>
+            {rooms.map ((room, idx) => ( 
+                <ListGroup.Item key={idx}>{room}</ListGroup.Item>
+            ))}
+        </ListGroup>
+        <h2>Members</h2>
+    </>
   )
 }
 
-export default Sidebar
+export default Sidebar;
