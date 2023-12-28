2af604191b31ca164e6246c233f15d5c2aa0e468
Shinji404 <20521981@gm.uit.edu.vn>
2023-12-28 07:18:14 +0700

    add test

--- src/services/appApi.js
+++ src/services/appApi.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const appApi = createApi({
     reducerPath: "appApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://itchat-backend2.onrender.com",
+        baseUrl: "http://localhost:5001",
     }),
 
     endpoints: (builder) => ({
